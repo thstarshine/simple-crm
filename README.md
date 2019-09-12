d8ce3ade3ded575cb6e0b96587cfd61f183cf3aa
starshine <th.starshine@gmail.com>
2019-09-12 15:24:07 +0800

    1. add fastify server 2. add prettier, eslint

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # simple-crm
+
 A simple CRM.
+
+## Run
+
+### Run server
+
+`yarn start-server`
+
+### Run front end
+
+`yarn start`
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,60 +1,53 @@
-import React from 'react'
-import { push } from 'connected-react-router'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import {
-  increment,
-  incrementAsync,
-  decrement,
-  decrementAsync
-} from '../../modules/counter'
+import React from 'react';
+import { push } from 'connected-react-router';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { increment, incrementAsync, decrement, decrementAsync } from '../../modules/counter';
 
 const Home = props => (
-  <div>
-    <h1>Home</h1>
-    <p>Count: {props.count}</p>
+    <div>
+        <h1>Home</h1>
+        <p>Count: {props.count}</p>
 
-    <p>
-      <button onClick={props.increment}>Increment</button>
-      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>
-        Increment Async
-      </button>
-    </p>
+        <p>
+            <button onClick={props.increment}>Increment</button>
+            <button onClick={props.incrementAsync} disabled={props.isIncrementing}>
+                Increment Async
+            </button>
+        </p>
 
-    <p>
-      <button onClick={props.decrement}>Decrement</button>
-      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>
-        Decrement Async
-      </button>
-    </p>
+        <p>
+            <button onClick={props.decrement}>Decrement</button>
+            <button onClick={props.decrementAsync} disabled={props.isDecrementing}>
+                Decrement Async
+            </button>
+        </p>
 
-    <p>
-      <button onClick={() => props.changePage()}>
-        Go to about page via redux
-      </button>
-    </p>
-  </div>
-)
+        <p>
+            <button onClick={() => props.changePage()}>Go to about page via redux</button>
+        </p>
+    </div>
+);
 
 const mapStateToProps = ({ counter }) => ({
-  count: counter.count,
-  isIncrementing: counter.isIncrementing,
-  isDecrementing: counter.isDecrementing
-})
+    count: counter.count,
+    isIncrementing: counter.isIncrementing,
+    isDecrementing: counter.isDecrementing,
+});
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      increment,
-      incrementAsync,
-      decrement,
-      decrementAsync,
-      changePage: () => push('/about-us')
-    },
-    dispatch
-  )
+    bindActionCreators(
+        {
+            increment,
+            incrementAsync,
+            decrement,
+            decrementAsync,
+            changePage: () => push('/about-us'),
+        },
+        dispatch,
+    );
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home)
+    mapStateToProps,
+    mapDispatchToProps,
+)(Home);
